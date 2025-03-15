bc4d2620ea143258dddd567ea75286555ec78b97
JoowonKim3642 <joowon.kim@g.austincc.edu>
2025-03-15 22:41:04 +0000

    Q4

--- src/question_4/question4.cpp
+++ src/question_4/question4.cpp
@@ -4,16 +4,19 @@ bool test_config()
 }
 
 int get_seconds(int seconds_since_1970){
-    return seconds_since_1970%60;
+    int result = seconds_since_1970 % 3600;
+    int remainder = result % 60;
+    return remainder;
 }
 
 int get_minutes(int seconds_since_1970){
-    int seconds = get_seconds(seconds_since_1970);
-    return ((seconds_since_1970 - seconds)/60)%60;
+    int result = seconds_since_1970 / 60;
+    int remainder = result % 60;
+    return remainder;
 }
 
 int get_hours(int seconds_since_1970){
-    int seconds = get_seconds(seconds_since_1970);
-    int minutes = get_minutes(seconds_since_1970)*60;
-    return ((seconds_since_1970 - minutes - seconds)/3600)%24;
+    int result = seconds_since_1970 / 3600;
+    int remainder = result % 24;
+    return remainder;
 }
