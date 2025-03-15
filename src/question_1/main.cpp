2ea9d39bf3cb77dc399dd5c0d2af2f2b66d6fcae
JoowonKim3642 <joowon.kim@g.austincc.edu>
2025-03-15 19:38:13 +0000

    Q1

--- src/question_1/main.cpp
+++ src/question_1/main.cpp
@@ -1,6 +1,39 @@
+#include "question1.h"
+#include <iostream>
+#include <vector>
 
-int main()
+using std::cin; using std::cout;
+
+int main() 
 {
+    int user_input;
+    int choice;
+    std::vector<int> prime_list;
 
-    return 0;
+    do
+    {
+        cout << "Please enter number in range of 1 to 60: ";
+        cin >> user_input;
+        
+        if (user_input < 1 || user_input > 60) {
+            cout << "\nInput out of range\n";
+        }
+        else {
+            prime_list = get_primes(user_input);
+
+            cout << "The list of primes preceding " << user_input << " are ";
+    
+            for (int number: prime_list)
+                cout << number << " ";
+        
+            cout << "\n";
+            cout << "1 - Stay\n";
+            cout << "2 - Exit\n";
+            cout << "Enter your choice: ";
+            cin >> choice;
+        }
+        
+    } while (choice != 2);
+    
+	return 0;
 }
--- test/question_test_1/question_tests_1.cpp
+++ test/question_test_1/question_tests_1.cpp
@@ -10,3 +10,18 @@ TEST_CASE("test")
 {
 	REQUIRE(test_config() == true);
 }
+
+TEST_CASE("Verify get_primes")
+{
+	std::vector condition = {2, 3, 5, 7};
+	CHECK(get_primes(10) == condition);
+
+	condition.push_back(11); condition.push_back(13);
+	CHECK(get_primes(15) == condition);
+
+	condition.push_back(17); condition.push_back(19); condition.push_back(23); condition.push_back(29);
+	CHECK(get_primes(30) == condition);
+
+	condition.push_back(31); condition.push_back(37); condition.push_back(41); condition.push_back(43); condition.push_back(47);
+	CHECK(get_primes(50) == condition);
+}
