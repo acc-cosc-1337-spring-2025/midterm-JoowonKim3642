687d61a57e98c57ca8da04449f9bbec49918f799
JoowonKim3642 <joowon.kim@g.austincc.edu>
2025-03-15 20:13:45 +0000

    Q2

--- src/question_2/main.cpp
+++ src/question_2/main.cpp
@@ -1,6 +1,31 @@
+#include<iostream>
+#include"question2.h"
+using namespace std;
 
 int main()
 {
+    int user_input;
+    int choice;
 
-    return 0;
+    do
+    {
+        cout << "Please enter number in range of 1 to 10: ";
+        cin >> user_input;
+        
+        if (user_input < 1 || user_input > 10) {
+            cout << "\nInput out of range\n";
+        }
+        else {
+            std::string result = get_factorial_sequence(user_input);
+
+            cout << "Factorial Sequence: " << result << endl;
+            cout << "1 - Stay\n";
+            cout << "2 - Exit\n";
+            cout << "Enter your choice: ";
+            cin >> choice;
+        }
+        
+    } while (choice != 2);
+    
+	return 0;
 }
--- src/question_2/question2.cpp
+++ src/question_2/question2.cpp
@@ -4,3 +4,18 @@ bool test_config()
 {
     return true;
 }
+
+std::string get_factorial_sequence(int num) 
+{
+    std::string sequence = "1";
+    int factorial = 1;
+
+    for (int i = 2; i <= num; ++i) 
+    {
+        sequence += "x" + std::to_string(i);
+        factorial *= i;
+    }
+
+    sequence += "=" + std::to_string(factorial);
+    return sequence;
+}
